c031edf34d03f9cc322439702322ad310033b36d
jojowabbit <jojowabbit@gmail.com>
2020-09-05 09:58:28 +0800

    Router setup

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
-  return <h1>ProductDetails</h1>;
+  let { id } = useParams();
+  console.log(id);
+  return <h1>ProductDetails Product id: {id}</h1>;
 };
 
 export default ProductDetails;

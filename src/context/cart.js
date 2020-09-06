26eb1ce1379ecfa8746ac1a2790dd80712db83be
jojowabbit <jojowabbit@gmail.com>
2020-09-06 09:13:39 +0800

    Cart Page rendering

--- src/context/cart.js
+++ src/context/cart.js
@@ -5,7 +5,7 @@ import localCart from "../utils/localCart";
 export const CartContext = React.createContext();
 
 export function CartProvider({ children }) {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(localCart);
   const [total, setTotal] = useState(0);
   const [cartItems, setCartItems] = useState(0);
 
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -13,5 +13,22 @@ export default function Cart() {
     return <EmptyCart />;
   }
 
-  return <h1>Cart</h1>;
+  return (
+    <section className="section cart-items">
+      <h2>your cart</h2>
+      {cart.map((item) => {
+        return <CartItem key={item.id} {...item} />;
+      })}
+      <h2>total: RM {total}</h2>
+      {user ? (
+        <Link to="/checkout" className="btn btn-primary btn-block">
+          checkout
+        </Link>
+      ) : (
+        <Link to="/login" className="btn btn-primary btn-block">
+          logn
+        </Link>
+      )}
+    </section>
+  );
 }

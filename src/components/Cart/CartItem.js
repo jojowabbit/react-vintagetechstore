1b2445f169a26e9e16f535372c215fed8bf9fa38
jojowabbit <jojowabbit@gmail.com>
2020-09-06 09:23:03 +0800

    CartItem component

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,42 @@
 import React from "react";
+import { FaAngleUp, FaAngleDown } from "react-icons/fa";
 
-const CartItem = () => {
-  return <h1>CartItem</h1>;
+import { CartContext } from "../../context/cart";
+
+const CartItem = ({ id, title, price, image, amount }) => {
+  return (
+    <article className="cart-item">
+      <img src={image} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h5>RM {price}</h5>
+        <button
+          type="button"
+          className="cart-btn remove-btn"
+          onClick={() => console.log("item removed")}
+        >
+          remove
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          className="cart-btn amount-btn"
+          onClick={() => console.log("amount ++")}
+        >
+          <FaAngleUp />
+        </button>
+        <p className="item-amount">{price}</p>
+        <button
+          type="button"
+          className="cart-btn amount-btn"
+          onClick={() => console.log("amount --")}
+        >
+          <FaAngleDown />
+        </button>
+      </div>
+    </article>
+  );
 };
 
 export default CartItem;

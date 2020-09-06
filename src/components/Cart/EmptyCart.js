6f9a2c755cdcb0e1272774baf2e87e01a50262c2
jojowabbit <jojowabbit@gmail.com>
2020-09-06 09:07:10 +0800

    EmptyCart component setup

--- src/components/Cart/EmptyCart.js
+++ src/components/Cart/EmptyCart.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const EmptyCart = () => {
-  return <h1>EmptyCart</h1>;
+  return (
+    <section className="section empty-cart">
+      <h2>empty cart...</h2>
+      <Link to="/products" className="btn btn-primary">
+        fill in
+      </Link>
+    </section>
+  );
 };
 
 export default EmptyCart;
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,17 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 
 import { CartContext } from "../context/cart";
+import EmptyCart from "../components/Cart/EmptyCart";
+import CartItem from "../components/Cart/CartItem";
+//import { UserContext } from "../context/user";
 
 export default function Cart() {
   const { cart, total } = useContext(CartContext);
-  console.log(total, cart);
+  const user = false;
+  if (cart.length === 0) {
+    return <EmptyCart />;
+  }
+
   return <h1>Cart</h1>;
 }

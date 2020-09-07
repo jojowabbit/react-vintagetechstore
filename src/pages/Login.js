2f07447e1e8e74b797fc6d8166ba010640db6969
jojowabbit <jojowabbit@gmail.com>
2020-09-07 14:24:25 +0800

    Login page setup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+// strapi function
+// handle user
+import { useHistory } from "react-router-dom";
 
 export default function Login() {
+  const history = useHistory();
+  // setup user context
+
+  // state values
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("default");
+  const [isMember, setIsMember] = useState(true);
+
+  let isEmpty = false;
+
+  const toggleMember = () => {};
+
+  const handleSubmit = async (e) => {};
+
   return <h1>Login</h1>;
 }

bd1a1a47ff58b6799d217db7f4ed68ef820b12ec
jojowabbit <jojowabbit@gmail.com>
2020-09-07 14:37:34 +0800

    Login page jsx

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -17,7 +17,72 @@ export default function Login() {
 
   const toggleMember = () => {};
 
-  const handleSubmit = async (e) => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
 
-  return <h1>Login</h1>;
+  return (
+    <section className="section form">
+      <h2 className="section-title">{isMember ? "sign in" : "register"}</h2>
+      <form className="login-form">
+        {/* single input */}
+        <div className="form-control">
+          <label htmlFor="email">email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        {/* end of single input */}
+        {/* single input */}
+        <div className="form-control">
+          <label htmlFor="password">password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {/* end of single input */}
+        {/* single input */}
+        {!isMember && (
+          <div className="form-control">
+            <label htmlFor="username">username</label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+        )}
+        {/* end of single input */}
+        {/* empty form text */}
+        {isEmpty && <p className="form-empty">please fill out all fields</p>}
+        {/* submit button */}
+        {!isEmpty && (
+          <button
+            type="submit"
+            className="btn btn-block btn-primary"
+            onClick={handleSubmit}
+          >
+            submit
+          </button>
+        )}
+        {/* register link */}
+        <p className="register-link">
+          {isMember ? "need to register" : "already a member"}
+          <button type="button" onClick={handleSubmit}>
+            click here
+          </button>
+        </p>
+      </form>
+    </section>
+  );
 }
